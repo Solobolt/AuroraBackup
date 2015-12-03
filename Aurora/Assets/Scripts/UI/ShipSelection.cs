65e4587e96061eaf5dcd28d2cad7676b38065287
Solobolt <solobolt@gmail.com>
2015-12-03 18:58:06 +1000

    Made ship models visable in the SHip selection UI

--- Aurora/Assets/Scripts/UI/ShipSelection.cs
+++ Aurora/Assets/Scripts/UI/ShipSelection.cs
@@ -6,10 +6,16 @@ public class ShipSelection : MonoBehaviour {
     public static int P2Ship = 1;
 
 
+    public GameObject P1AtkModel;
+    public GameObject P1DefModel;
+    public GameObject P2AtkModel;
+    public GameObject P2DefModel;
+
     // Use this for initialization
     void Start () {
-	
-	}
+        P1DefModel.SetActive(false);
+        P2DefModel.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,12 +26,14 @@ public class ShipSelection : MonoBehaviour {
     public void P1SetShipType(int ship)
     {
         P1Ship = ship;
+        ChangeP1Model(ship);
     }
 
     //Sets the player 1st players ship type
     public void P2SetShipType(int ship)
     {
         P2Ship = ship;
+        ChangeP2Model(ship);
     }
 
     public void PlayLevel()
@@ -36,5 +44,46 @@ public class ShipSelection : MonoBehaviour {
     public void MoveToCustom()
     {
         Application.LoadLevel("CustomizationMenu");
+    }
+
+
+    public void ChangeP1Model(int modelNumber)
+    {
+        switch(modelNumber)
+        {
+            case 1:
+                P1AtkModel.SetActive(true);
+                P1DefModel.SetActive(false);
+                break;
+
+            case 2:
+                P1AtkModel.SetActive(false);
+                P1DefModel.SetActive(true);
+                break;
+            default:
+                P1AtkModel.SetActive(true);
+                P1DefModel.SetActive(false);
+                break;
+        }
+    }
+
+    public void ChangeP2Model(int modelNumber)
+    {
+        switch (modelNumber)
+        {
+            case 1:
+                P2AtkModel.SetActive(true);
+                P2DefModel.SetActive(false);
+                break;
+
+            case 2:
+                P2AtkModel.SetActive(false);
+                P2DefModel.SetActive(true);
+                break;
+            default:
+                P2AtkModel.SetActive(true);
+                P2DefModel.SetActive(false);
+                break;
+        }
     }
 }
